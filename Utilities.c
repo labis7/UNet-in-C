96409b4456878fbc840942105031616e258a5f39
labis7 <labis_skoufis@hotmail.com>
2020-06-18 17:25:17 +0300

    Filters and bias initializing function completed!

--- Utilities.c
+++ Utilities.c
@@ -11,9 +11,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+////////////// Function Naming init. ////////////////
+float Random_Normal(int loc, float scale);
 float *****testff(float ****t);
-void Initialize_parameters(struct init_param_ *ptr_init_param);
+float ****make_4darray(int num,int channels,int dim);
 float ***make_3darray(int channels,int dim);
+////////////////////////////////////////////////////
+
 struct act_func_data_
 {
 	/*
@@ -29,6 +34,15 @@ struct act_func_data_
 
 }act_func_data;
 
+struct GP_arrays_ //General purpose arrays
+{
+	float *dim1;
+	float **dim2;
+	float ***dim3;
+	float ****dim4;
+	float *****dim5;
+}GP_arrays;
+
 struct init_param_
 {
 	/*
@@ -39,13 +53,124 @@ struct init_param_
 	 * F_dc: This matrix contains the decoder upsampling transposed convolution filters.Its type is the same as Filters matrix.
 	 */
 	int layers, num_f, trim;
-	float *****filters,**bias, *****f_dc;
+	float *****filters,**bias, *****f_dc,**b_dc;
 
 }init_param;
 
 void Initialize_parameters(struct init_param_ *ptr_init_param)
 {
+	int layers = ptr_init_param->layers;//number of layers(just the encoder-downsampling number)
+	int num_f = ptr_init_param->num_f;  //initial number of filters(they will be doubled for each layer)
+	float trim = ptr_init_param->trim;  //weight scale of the values
+	float *****filters = (float *****)malloc((layers*2*2-1)*sizeof(float ****));
+	float **bias = (float **)malloc((layers*2*2-1)*sizeof(float *));
+	float *****f_dc = (float *****)malloc((layers-1)*sizeof(float ****));
+	float **b_dc = (float **)malloc((layers-1)*sizeof(float *));
 
+	int ch_in = 1; //number of initial channels input from the input image
+	int loc = 0; //normal distribution around zero
+
+	int last_pos=0;
+	//Building the downsampling/encoder filters first.(5 layers*2 filters each)
+	for (int i=0; i<layers; i++)
+	{
+		if(i != 0) //after 1st iteration, double the amount of filters of each layer
+			num_f = num_f*2; //*16*, 32, 64, 128, 256
+		//Building f1
+		float ****f1 = make_4darray(num_f, ch_in, 3);//3x3 filter alwars for the simple convolution
+		float *b1 = (float *)malloc(num_f*sizeof(float));
+		for (int x=0;x<1;x++)
+		{
+			for (int y=0;y<2;y++)
+				for (int z=0;z<3;z++)
+					for(int w=0;w<3;w++)
+					{
+						f1[x][y][z][w] = Random_Normal(loc, trim);
+					}
+			b1[x] =  Random_Normal(loc, trim);
+		}
+
+		/*
+		 * Very important! Next filter will have as input the output/channels of the previous filter
+		 * More : When we apply a number of filters on an image, we create num_f channels on the image result.
+		 * So each filter must have input dim same as the input image channels, output dim = channels we want to occur after conv.
+		 */
+		ch_in = num_f;
+
+		float ****f2 = make_4darray(num_f, ch_in, 3);//3x3 filter alwars for the simple convolution
+		float *b2 = (float *)malloc(num_f*sizeof(float));
+		for (int x=0;x<1;x++)
+		{
+			for (int y=0;y<2;y++)
+				for (int z=0;z<3;z++)
+					for(int w=0;w<3;w++)
+					{
+						f2[x][y][z][w] = Random_Normal(loc, trim);
+					}
+			b2[x] = Random_Normal(loc, trim);
+		}
+		filters[2*i] = f1;
+		filters[2*i +1] = f2;
+		bias[2*i] = b1;
+		bias[2*i +1] = b2;
+		last_pos = i; //last position of the i that shows the current layer, we will need it later so we keep saving filters and
+		//bias sequencially.
+	}
+
+	for (int i = 1 ; i < layers; i++)
+	{
+		num_f = (int)num_f/2;//It will be always power of number of filters,so the division will give back integer
+
+		float ****fdc = make_4darray(num_f, ch_in, 2);//2x2 filter always for the upsampling/transposed convolution
+		float *bdc = (float *)malloc(num_f*sizeof(float));
+		float ****f1 = make_4darray(num_f, ch_in, 3);//3x3 filter always for the simple convolution
+		float *b1 = (float *)malloc(num_f*sizeof(float));
+		for (int x=0;x<1;x++)
+		{
+			for (int y=0;y<2;y++)
+			{
+				for (int z=0;z<3;z++)
+				{
+					for(int w=0;w<3;w++)
+						f1[x][y][z][w] = Random_Normal(loc, trim);
+				}
+				for (int z=0;z<2;z++)
+				{
+					for(int w=0;w<2;w++)
+						fdc[x][y][z][w] = Random_Normal(loc, trim);
+				}
+			}
+			bdc[x] =  Random_Normal(loc, trim);
+			b1[x] = Random_Normal(loc, trim);
+		}
+
+		ch_in = num_f ;
+
+		float ****f2 = make_4darray(num_f, ch_in, 3);//3x3 filter always for the simple convolution
+		float *b2 = (float *)malloc(num_f*sizeof(float));
+		for (int x=0;x<1;x++)
+		{
+			for (int y=0;y<2;y++)
+				for (int z=0;z<3;z++)
+					for(int w=0;w<3;w++)
+					{
+						f2[x][y][z][w] = Random_Normal(loc, trim);
+					}
+			b2[x] =  Random_Normal(loc, trim);
+		}
+
+		filters[last_pos*2] = f1;
+		filters[last_pos*2 + 1] = f2;
+		bias[last_pos*2] = b1;
+		bias[last_pos*2 + 1] = b2;
+		f_dc[i-1] = fdc;
+		b_dc[i] = bdc;
+		last_pos++;
+	}
+	ptr_init_param->filters = filters;
+	ptr_init_param->bias = bias;
+	ptr_init_param->f_dc = f_dc;
+	ptr_init_param->b_dc = b_dc;
 }
 
 float ***Activation_Function(struct act_func_data_ *act_func_data)
@@ -140,7 +265,19 @@ float ****make_4darray(int num,int channels,int dim)
 	return array;
 }
 
+float Random_Normal(int loc, float scale)
+{
+   // return a normally distributed random value
+	scale = 1;
+	loc=0;
+	float v1 = ( (float)(rand()) + 1. )/( (float)(RAND_MAX) + 1. ); //random gen
+	float v2 = ( (float)(rand()) + 1. )/( (float)(RAND_MAX) + 1. );//random gen
+	return ((cos(2*3.14*v2)*sqrt(-2.*log(v1)))*scale + loc);
+}
 int main(void) {
+	time_t t;
+	srand((unsigned) time(&t));
+
 	puts("Hello World!"); /* prints Hello World! */
 
 	int channels,code,dim;
@@ -166,8 +303,28 @@ int main(void) {
 	float ***res = Activation_Function(ptr_act_func_data);
 
 	float ****test = make_4darray(1,2,3);
-	//struct act_func_data_ *ptr_init_params = &init_params;
+	struct init_param_ *ptr_init_param = &init_param;
+
+	//Initialize_parameters(ptr_init_param);
+	float tt=(float)3/2;
+	printf("%f", tt);
 
 	return EXIT_SUCCESS;
 }
 
+float *****testff(float ****t){
+	float ****t2 = make_4darray(1,2,3);
+	for (int i=0;i<1;i++)
+		for (int j=0;j<2;j++)
+			for (int k=0;k<3;k++){
+				for(int y=0;y<3;y++)
+				{
+					t[i][j][k][y]=i+j+y+k;
+					t2[i][j][k][y]=2*i+2*j+2*y+2*k;
+				}
+			}
+	float *****t_array = (float *****)malloc(2*sizeof(float ****));
+	t_array[0] = t;
+	t_array[1] = t2;
+	return t_array;
+}
